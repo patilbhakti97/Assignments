190bf6d6f91bcc8e3d53e56782e05a5f617035f8
patilbhakti97 <114335887+patilbhakti97@users.noreply.github.com>
2022-10-17 20:28:30 +0530

    Add files via upload

---
+++ assig2.js
@@ -0,0 +1,14 @@
+// let name="Ilya";
+// console.log('hello' +1);
+// console.log('hello'+'name');
+// console.log('hello'+name);
+
+// prompt("This is Bhakti");
+// a=parseInt();
+// document.write('your name is'+a)
+
+let a=prompt('enter your marks for a');
+let b=prompt('enter your marks for b');
+let total=a+b;
+
+document.write('show total'+total);
