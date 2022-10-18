5e1cea5de4d191ff641bcbd2543406ca1d2fe3ce
patilbhakti97 <114335887+patilbhakti97@users.noreply.github.com>
2022-10-18 23:12:30 +0530

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,15 @@
+var a="admin";
+console.log(a);
+var b="name";
+console.log(b);
+var name="john";
+console.log("name:"+name);
+console.log("admin:"+name);
+console.log("alert(admin):"+name);
+
+let planet="Earth";
+console.log("Our planet name:"+planet);
+let visitor="Bhakti";
+console.log("Current visitor name:"+visitor);
+
+
---
+++ assignq1.js
@@ -0,0 +1,9 @@
+// Q-1 
+let a=prompt("What is the official name of Javascript?")
+let result;
+if (result=='ECMAScript'){
+    document.write("correct answer");
+}
+else {
+    document.write("Incorrect answer");
+}
