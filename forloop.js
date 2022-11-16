eef9bba13f68cf818900e77b2da21ef47a048d4a
patilbhakti97 <114335887+patilbhakti97@users.noreply.github.com>
2022-11-16 19:24:57 +0530

    Add files via upload

--- forloop.js
+++ forloop.js
@@ -1,8 +1,9 @@
-let num=prompt("Enter a number","");
-let i=0;
-for(i=1;i<=10;i++)
-{
-    document.write("<div>");
-    document.write(num+"*"+i+"="+i*num);
-    document.write("</div>");
-}   
+let a=prompt("Enter value of a")
+
+    if((a%2)===0){
+    alert("number is even")
+    }
+    else{
+        alert("number is odd")
+    }
+
